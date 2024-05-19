0da40e95f5ab5de8456fdb9a733477ef8e097ef6
QRodriguez443 <141974332+QRodriguez443@users.noreply.github.com>
2024-05-19 14:32:59 -0400

    First step

--- stream.py
+++ stream.py
@@ -1,1 +1,7 @@
-import pytube.streams
+from pytube import YouTube
+
+# Retrieve URL of video if user is currently on the relevant page
+url = "https://"
+youtube = YouTube(url)
+video = youtube.streams.first()
+streaming_url = video.url
