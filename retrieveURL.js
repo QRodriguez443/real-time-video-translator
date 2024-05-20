a0e26fa3e66aca1fd453947de2a710fd9c4e4ec9
QRodriguez443 <141974332+QRodriguez443@users.noreply.github.com>
2024-05-19 20:09:32 -0400

    scrapped idea

--- retrieveURL.js
+++
@@ -1,8 +0,0 @@
-// Get the user's current URL
-let url = window.location.href;
-
-// Check if the URL contains "youtube.com/watch"
-if (url.includes("youtube.com/watch")) {
-  // Do something with the URL
-  console.log(url);
-}
